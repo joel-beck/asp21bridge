33004999f4e7beabfe33eeffe15f55220105db86
Nicolai Bäuerle <nicolai.baeuerle@stud.uni-goettingen.de>
2021-06-07 15:27:37 +0200

    small changes on validate_input() function pt.1

--- R/gibbs-helpers.R
+++ R/gibbs-helpers.R
@@ -91,7 +91,7 @@ validate_input <- function(m, X, Z, y,
     for (l in 1:length(input_list)) {
       if (is.null(input_list[[l]])) {
         mod <- TRUE
-        assign(names(input_list[l]), mod_name_list[[l]])
+        return(assign(names(input_list[l]), mod_name_list[[l]]))
       }
     }
 
